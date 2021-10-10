3052cd3a548b4864a7f8348b7e3527bd0ea7babd
Jonathan Lam <jlam55555@gmail.com>
2021-10-10 04:09:55 -0400

    added more test cases to fixture

--- pset1/src/PsetOne/PartThree/Fixture.hs
+++ pset1/src/PsetOne/PartThree/Fixture.hs
@@ -8,7 +8,6 @@ import           PsetOne.Util
 
 -- TODO: generate some test queries to show functionality
 -- TODO: write-up about everything
--- TODO: add more test cases
 
 createFixture = do
   resetSchema
@@ -63,49 +62,115 @@ createFixture = do
         )
       ]
   insertClockTimes
-    [ clockIn marsha $ makeTime 2021 10 03 09 00 00
-    , clockOut marsha $ makeTime 2000 10 03 17 01 00
-    , clockIn marsha $ makeTime 2000 10 04 08 58 00
-    , clockOut marsha $ makeTime 2000 10 04 17 00 00
-    , clockIn marsha $ makeTime 2000 10 05 09 00 00
-    , clockOut marsha $ makeTime 2000 10 05 17 00 00
-    , clockIn marsha $ makeTime 2000 10 06 09 00 00
-    , clockOut marsha $ makeTime 2000 10 06 17 00 00
-    , clockIn willard $ makeTime 2000 10 03 08 58 00
-    , clockOut willard $ makeTime 2000 10 03 17 00 00
-    , clockIn willard $ makeTime 2000 10 04 08 58 00
-    , clockOut willard $ makeTime 2000 10 04 17 00 00
-    , clockIn bryon $ makeTime 2000 10 05 09 00 00
-    , clockOut bryon $ makeTime 2000 10 05 17 00 00
-    , clockIn bryon $ makeTime 2000 10 06 09 00 00
-    , clockOut bryon $ makeTime 2000 10 06 17 00 00
+    [ clockIn marsha $ makeTime 0002021 10 03 09 00 00
+    , clockOut marsha $ makeTime 002021 10 03 17 01 00
+    , clockIn marsha $ makeTime 0002021 10 04 08 58 00
+    , clockOut marsha $ makeTime 002021 10 04 17 00 00
+    , clockIn marsha $ makeTime 0002021 10 05 09 00 00
+    , clockOut marsha $ makeTime 002021 10 05 17 00 00
+    , clockIn marsha $ makeTime 0002021 10 06 09 00 00
+    , clockOut marsha $ makeTime 002021 10 06 17 00 00
+    , clockIn willard $ makeTime 002021 10 03 08 58 00
+    , clockOut willard $ makeTime 02021 10 03 17 00 00
+    , clockIn willard $ makeTime 002021 10 04 08 58 00
+    , clockOut willard $ makeTime 02021 10 04 17 00 00
+    , clockIn bryon $ makeTime 00002021 10 05 09 00 00
+    , clockOut bryon $ makeTime 0002021 10 05 17 00 00
+    , clockIn bryon $ makeTime 00002021 10 06 09 00 00
+    , clockOut bryon $ makeTime 0002021 10 06 17 00 00
+    , clockIn chanelle $ makeTime 02021 10 03 09 00 00
+    , clockOut chanelle $ makeTime 2021 10 03 05 00 00
+    , clockIn chanelle $ makeTime 02021 10 04 09 00 00
+    , clockOut chanelle $ makeTime 2021 10 04 05 00 00
+    , clockIn chanelle $ makeTime 02021 10 05 09 00 00
+    , clockOut chanelle $ makeTime 2021 10 05 05 00 00
+    , clockIn chanelle $ makeTime 02021 10 06 09 00 00
+    , clockOut chanelle $ makeTime 2021 10 06 05 00 00
+    , clockIn chanelle $ makeTime 02021 10 07 09 00 00
+    , clockOut chanelle $ makeTime 2021 10 07 05 00 00
+    , clockIn vikki $ makeTime 00002021 10 03 09 00 00
+    , clockOut vikki $ makeTime 0002021 10 03 05 00 00
+    , clockIn vikki $ makeTime 00002021 10 07 09 00 00
+    , clockOut vikki $ makeTime 0002021 10 07 05 00 00
     ]
-  [rsv1, rsv2] <- insertReservations
-    [ (hershel, andiamo   , marsha, makeTime 2020 10 03 12 01 52, 50)
-    , (hershel, coolChange, marsha, makeTime 2020 10 04 12 01 52, 50)
-    ]
+  [rsv1, rsv2, _, _, _, rsv3, _, _, rsv4, _, _, _, _, rsv5] <-
+    insertReservations
+      [ (hershel, andiamo, marsha, makeTime 2021 10 03 12 00 00, 50)
+      , (hershel, coolChange, marsha, makeTime 2021 10 04 12 00 00, 50)
+      , (hershel, shenanigans, marsha, makeTime 2021 10 05 12 00 00, 50)
+      , (hershel, knotOnCall, vikki, makeTime 2021 10 07 12 00 00, 50)
+      , (joenn, grace, chanelle, makeTime 2021 10 03 11 00 00, 50)
+      , (joenn, grace, vikki, makeTime 2021 10 04 11 00 00, 50)
+      , (joenn, knotOnCall, bryon, makeTime 2021 10 05 11 00 00, 50)
+      , (vania, knotOnCall, chanelle, makeTime 2021 10 03 11 00 00, 50)
+      , (vania, coolChange, chanelle, makeTime 2021 10 05 11 00 00, 50)
+      , (vania, andiamo, chanelle, makeTime 2021 10 07 11 00 00, 50)
+      , (katheryn, socialDistancing, vikki, makeTime 2021 10 07 11 00 00, 50)
+      , (shanika, socialDistancing, vikki, makeTime 2021 10 06 11 00 00, 50)
+      , (li, socialDistancing, willard, makeTime 2021 10 04 11 00 00, 50)
+      , (zachariah, shenanigans, willard, makeTime 2021 10 03 11 00 00, 50)
+      ]
   insertEquipmentSales
-    [ (hershel, marineRotationPlate, 1, makeTime 2020 10 03 11 50 00)
-    , (hershel, boatHookEnd        , 3, makeTime 2020 10 03 11 50 00)
+    [ (hershel  , marineRotationPlate, 1, makeTime 2021 10 03 11 50 00)
+    , (hershel  , boatHookEnd        , 3, makeTime 2021 10 03 11 50 00)
+    , (marinda  , eyestrapKit        , 1, makeTime 2021 10 05 11 50 00)
+    , (madeleine, boatHookEnd        , 2, makeTime 2021 10 04 11 50 00)
+    , (clara    , boatHookEnd        , 1, makeTime 2021 10 03 11 50 00)
     ]
   insertIncidents
     [ ( rsv1
-      , makeTime 2020 10 03 13 00 02
-      , 3
+      , makeTime 2021 10 03 13 00 02
+      , 7
       , "boat ran aground"
       , marsha
-      , False
+      , True
       , "fixed hull"
-      , Nothing
+      , Just 100000
       )
     , ( rsv1
-      , makeTime 2020 10 03 14 44 02
-      , 3
+      , makeTime 2021 10 03 14 44 02
+      , 10
       , "Boat sunk"
-      , marsha
+      , chanelle
       , False
       , "bought new boat"
       , Just 1250000
+      )
+    , ( rsv2
+      , makeTime 2021 10 04 14 44 02
+      , 2
+      , "Caught a big fish"
+      , vikki
+      , True
+      , "N/A"
+      , Nothing
+      )
+    , ( rsv3
+      , makeTime 2021 10 04 14 44 02
+      , 3
+      , "Man overboard"
+      , chanelle
+      , True
+      , "N/A"
+      , Nothing
+      )
+    , ( rsv4
+      , makeTime 2021 10 05 14 44 02
+      , 3
+      , "Damage to propeller"
+      , chanelle
+      , True
+      , "Bought new propeller"
+      , Just 5000
+      )
+    , ( rsv5
+      , makeTime 2021 10 03 14 44 02
+      , 3
+      , "Whale sighting"
+      , chanelle
+      , True
+      , "N/A"
+      , Nothing
       )
     ]
   pure ()
