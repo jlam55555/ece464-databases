9a8577673070b09c47f546df765e66fb77304530
Jonathan Lam <jlam55555@gmail.com>
2021-11-27 21:29:32 -0500

    added sample item queries

--- pset2/driver.lisp
+++ pset2/driver.lisp
@@ -12,6 +12,5 @@
    "gpu"
    "cpu"
    "computer"
-   "thinkstation"
    "sandals"
    "crocs"))
--- pset2/queries.js
+++ pset2/queries.js
@@ -3,20 +3,108 @@
 // To execute this file:
 // $ mongo localhost:27017/ece464_pset2 queries.js
 
+// Try changing this to one of the other queries.
+const query = "gpu";
+
 // Helper function to print out the results of a query.
 function dumpCursor(cursor) {
     while (cursor.hasNext()) {
         printjson(cursor.next());
     }
 }
+const sellers = db["seller_" + query.replace(" ", "_")];
+const items = db["item_" + query.replace(" ", "_")];
 
-let sellers = db.seller_gpu;
-let item = db.item_gpu;
-
-print("Examining collection: " + item);
+print("Examining collection: " + items);
 
 print("Item count:");
-printjson(item.count());
+printjson(items.count());
+
+print("Top ten most frequent sellers:");
+dumpCursor(items.aggregate([
+    {
+        $group: {
+            _id: "$seller-name",
+            "items-count": { $count: {} }
+        }
+    },
+    {
+        $sort: {
+            "items-count": -1
+        }
+    },
+    {
+        $project: {
+            seller: "$_id",
+            "items-count": true,
+            _id: false
+        }
+    },
+    {
+        $limit: 10
+    }
+]));
+
+print("Items shipping to the United States:");
+printjson(items.find({
+    "ships-to-locations.includes": "United States"
+}).count());
+
+print("Price distribution (only if priced in USD):");
+dumpCursor(items.aggregate([
+    {
+        $match: {
+            "itemprops.priceCurrency": "USD",
+        }
+    },
+    {
+        $project: {
+            "itemprops.price": true,
+            _id: false
+        }
+    },
+    {
+        $bucket: {
+            groupBy: "$itemprops.price",
+            boundaries: [
+                0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000,
+                2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000,
+                20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000,
+                100000, 200000, 300000, 400000, 500000
+            ],
+        }
+    },
+    {
+        $project: {
+            "bin-lower-bound": "$_id",
+            count: true,
+            _id: false,
+        }
+    }
+]));
+
+print("Distinct brands:");
+printjson(items.distinct("about-this-item.Brand:"));
+
+print("Average price of items with MPN '900-1G142-2520-000' (Nvidia FE 3060 Ti):");
+dumpCursor(items.aggregate([
+    {
+        $match: {
+            "about-this-item.MPN:": "900-1G142-2520-000"
+        }
+    },
+    {
+        $group: {
+            _id: null,
+            "avgPrice": { $avg: "$itemprops.price" },
+        }
+    },
+    {
+        $project: {
+            _id: false,
+        }
+    }
+]));
 
 print("Sellers count:");
 printjson(sellers.count());
